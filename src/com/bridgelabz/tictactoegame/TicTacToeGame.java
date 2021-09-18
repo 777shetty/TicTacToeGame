f044e2e6da6b91cc489f1e47629201bfd4ac1cdc
777shetty <shetty.chetu7@gmail.com>
2021-09-18 15:28:20 +0530

    InitialFile

---
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.tictactoegame;
+
+public class TicTacToeGame {
+	static char board[] = new char[10];
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TicTacToe Game!");
+		for (int index = 1; index < board.length; index++) {
+			board[index] = ' ';
+		}
+	}
+
+}
